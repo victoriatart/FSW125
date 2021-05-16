065746ac15af602bce671b9334bd605f339205a7
victoriatart <68974738+victoriatart@users.noreply.github.com>
2021-05-15 23:08:37 -0400

    Add files via upload

---
+++ Week1/Math/app.js
@@ -0,0 +1,6 @@
+const mathObj = require('./math')
+
+console.log(mathObj.sum(10, 2)); //12
+console.log(mathObj.subtract(20, 10)); //10
+console.log(mathObj.multiplication(20, 10)); //200
+console.log(mathObj.division(50, 5)); //10
---
+++ Week1/Math/math.js
@@ -0,0 +1,22 @@
+const sum = (a, b) => {
+    return a + b
+};
+
+const subtract = (c, d) => {
+    return c - d 
+};
+
+const multiplication = (e, f) => {
+    return e * f
+};
+
+const divison = (g, h) => {
+    return g / h
+};
+
+module.exports = {
+    sum: sum,
+    subtract: subtract,
+    multiplication: multiplication,
+    division: divison
+};
