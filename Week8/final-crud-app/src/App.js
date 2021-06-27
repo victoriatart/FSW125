bfc602cbde4c55d431f1434dbe4363ff05d9bebb
victoriatart <68974738+victoriatart@users.noreply.github.com>
2021-06-27 14:42:21 -0400

    Add files via upload

---
+++ Week8/final-crud-app/src/App.js
@@ -0,0 +1,20 @@
+
+import React from "react";
+import Results from "./components/results/Results";
+import Loading from "./components/Loading";
+import { ToastContainer } from "react-toastify";
+
+import "./App.css";
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  return (
+    <div>
+      <Results />
+      <Loading />
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
